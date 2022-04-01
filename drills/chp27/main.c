75943587b230321f18d5d21fe77a181e35b6bca1
Tóth Kristóf Imre <kristof01124@gmail.com>
2022-04-01 09:41:33 +0200

    Chapter 27

---
+++ drills/chp27/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void foo(char* p, int x) {
+    printf("p is %s and x is %d\n", p, x);
+}
+
+int main() {
+    printf("Hello World!\n");
+    char* a = "Hello",* b = "World!";
+    printf("%s %s\n", a,b);
+    foo("alma", 10);
+    foo("p", 0);
+}
